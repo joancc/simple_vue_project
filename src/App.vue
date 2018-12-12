9761860040d0f58a55a7d8f9224c45a5bf9e182b
Joan Cejudo <joan.cejudo@gestionix.com>
2018-12-12 12:04:56 -0600

    PRODUCTION

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Testing Production Release"/>
+    <HelloWorld msg="Master"/>
   </div>
 </template>
 
